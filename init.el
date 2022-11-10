14b9419309bca67b12116a7aa725b12f76870b71
Jake Tuero <jaketuero94@gmail.com>
2022-11-09 20:39:46 -0700

    Update init.el

--- init.el
+++ init.el
@@ -325,6 +325,14 @@ or the current buffer directory."
 (setq highlight-indent-guides-responsive 'top)   ; Responsive guide [nil,top,stack]
 
 ; -----------------------------------------------
+
+(use-package ace-window
+  :ensure t 
+  :bind ("M-o" . ace-window)
+  :delight 
+  :config (ace-window-display-mode 1)
+)
+; -----------------------------------------------
 ; themes
 (add-to-list 'load-path "~/.emacs.d/external/doomemacs-themes/")
 (require 'doom-themes)
